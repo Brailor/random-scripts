28e8eee4780d2e78edf0e4ad7be12a8abe270033
Viktor Ohád <viktor.ohad@protonmail.com>
2022-09-02 13:05:49 +0200

    add releaser script

---
+++ instructure/instui/create_release.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# This script should be run from the root of the instui git worktree directory.
+read -p "v8 or v7 release?: " release_number
+
+curr_time="$(date -u  +%Y-%m-%d)"
+worktree_name="${release_number}-release"
+branch_name="${worktree_name}-${curr_time}"
+
+echo "Creating git worktree..."
+git worktree add "$worktree_name" -b "$branch_name"
+echo "Worktree done, navigating into it.."
+cd "$worktree_name"
+echo "Running yarn install..."
+yarn
+echo "Running yarn bootstrap..."
+yarn bootstrap
+osascript -e "display notification \"Bootstrap for branch: $branch_name is completed!\" with title \"Bootstrap completed!\""
+echo "Bumping packages..."
+yarn bump
